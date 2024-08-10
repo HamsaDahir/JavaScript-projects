91b15f83c9c148be81353cb5bf797cbf2330ed66
HamsaDahir <hamzadahir3@gmail.com>
2024-08-10 11:51:32 +0100

    traffic light project

--- 19-traffic-lights/main.js
+++ 19-traffic-lights/main.js
@@ -3,25 +3,25 @@ let activeLight = 0;
 
 
 
-// setInterval(() => {
-//   changeLight();
-// }, 1000)
+setInterval(() => {
+  changeLight();
+}, 1000)
 
 
-// function changeLight() {
+function changeLight() {
   
-//   circles[activeLight].className = 'circle';
-//   activeLight++;
+  circles[activeLight].className = 'circle';
+  activeLight++;
 
-//   if (activeLight > 2) {
-//     activeLight = 0
-//   }
+  if (activeLight > 2) {
+    activeLight = 0
+  }
  
-//   const currentLight = circles[activeLight]
+  const currentLight = circles[activeLight]
 
-//   currentLight.classList.add(currentLight.getAttribute('color'))
+  currentLight.classList.add(currentLight.getAttribute('color'))
 
-// }
+}
 
-// changeLight()
+changeLight()
 
